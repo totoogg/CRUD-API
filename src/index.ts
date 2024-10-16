2b988c9c000b298f2e3212f989dcf837ecefa647
totoogg <goncharov.vova2014@gamil.com>
2024-10-16 20:03:14 +0300

    feat: add .env file

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,90 @@
 import { createServer } from 'http';
 import { users } from './db/index.ts';
+import 'dotenv/config';
 
-const PORT = 5000;
+const PORT = process.env.PORT;
 
 const server = createServer((req, res) => {
-  if (req.method === 'GET' && req.url === '/api/users') {
-    const response = users.getUsers();
+  try {
+    if (req.method === 'GET' && req.url === '/api/users') {
+      const response = users.getUsers();
 
-    if (response) {
+      if (response) {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+
+        return res.end(JSON.stringify(response));
+      } else {
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain');
+
+        return res.end('Server error');
+      }
+    } else if (req.method === 'GET' && req.url?.startsWith('/api/users/')) {
+      const userId = req.url.slice(11);
+      const user = users.getUserById(userId);
+
+      if (user === '400') {
+        res.statusCode = 400;
+        res.setHeader('Content-Type', 'text/plain');
+
+        return res.end('Invalid user id');
+      }
+
+      if (user === '404') {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/plain');
+
+        return res.end(`User with id ${userId} does not exist`);
+      }
+
       res.statusCode = 200;
       res.setHeader('Content-Type', 'application/json');
 
-      return res.end(JSON.stringify(response));
-    } else {
-      res.statusCode = 500;
-      res.setHeader('Content-Type', 'text/plain');
+      return res.end(JSON.stringify(user));
+    } else if (req.method === 'POST' && req.url === '/api/users') {
+      if (req.headers['content-type'] === 'application/json') {
+        let data = '';
 
-      return res.end('Server error');
-    }
-  } else if (req.method === 'GET' && req.url?.startsWith('/api/users/')) {
-    const userId = req.url.slice(11);
-    const user = users.getUserById(userId);
+        req.on('data', (chunk: string) => {
+          data += chunk;
+        });
 
-    if (user === '400') {
-      res.statusCode = 400;
-      res.setHeader('Content-Type', 'text/plain');
+        req.on('end', () => {
+          const user = users.addUser(JSON.parse(data));
 
-      return res.end('Invalid user id');
-    }
+          if (user === '400') {
+            res.statusCode = 400;
+            res.setHeader('Content-Type', 'text/plain');
 
-    if (user === '404') {
-      res.statusCode = 404;
-      res.setHeader('Content-Type', 'text/plain');
+            return res.end('Incorrect data for user creation');
+          }
 
-      return res.end(`User with id ${userId} does not exist`);
-    }
+          res.setHeader('Content-Type', 'application/json');
 
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
+          return res.end(JSON.stringify(user));
+        });
+      }
 
-    return res.end(JSON.stringify(user));
-  } else if (req.method === 'POST' && req.url === '/api/users') {
-    if (req.headers['content-type'] === 'application/json') {
+      return;
+    } else if (req.method === 'PUT' && req.url?.startsWith('/api/users/')) {
+      const userId = req.url.slice(11);
+      const user = users.getUserById(userId);
+
+      if (user === '400') {
+        res.statusCode = 400;
+        res.setHeader('Content-Type', 'text/plain');
+
+        return res.end('Invalid user id');
+      }
+
+      if (user === '404') {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/plain');
+
+        return res.end(`User with id ${userId} does not exist`);
+      }
+
       let data = '';
 
       req.on('data', (chunk: string) => {
@@ -49,7 +92,7 @@ const server = createServer((req, res) => {
       });
 
       req.on('end', () => {
-        const user = users.addUser(JSON.parse(data));
+        const user = users.updateUser(userId, JSON.parse(data));
 
         if (user === '400') {
           res.statusCode = 400;
@@ -59,80 +102,44 @@ const server = createServer((req, res) => {
         }
 
         res.setHeader('Content-Type', 'application/json');
-
         return res.end(JSON.stringify(user));
       });
-    }
 
-    return;
-  } else if (req.method === 'PUT' && req.url?.startsWith('/api/users/')) {
-    const userId = req.url.slice(11);
-    const user = users.getUserById(userId);
-
-    if (user === '400') {
-      res.statusCode = 400;
-      res.setHeader('Content-Type', 'text/plain');
-
-      return res.end('Invalid user id');
-    }
-
-    if (user === '404') {
-      res.statusCode = 404;
-      res.setHeader('Content-Type', 'text/plain');
-
-      return res.end(`User with id ${userId} does not exist`);
-    }
-
-    let data = '';
-
-    req.on('data', (chunk: string) => {
-      data += chunk;
-    });
-
-    req.on('end', () => {
-      const user = users.updateUser(userId, JSON.parse(data));
+      return;
+    } else if (req.method === 'DELETE' && req.url?.startsWith('/api/users/')) {
+      const userId = req.url.slice(11);
+      const user = users.getUserById(userId);
 
       if (user === '400') {
         res.statusCode = 400;
         res.setHeader('Content-Type', 'text/plain');
 
-        return res.end('Incorrect data for user creation');
+        return res.end('Invalid user id');
       }
 
-      res.setHeader('Content-Type', 'application/json');
-      return res.end(JSON.stringify(user));
-    });
+      if (user === '404') {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/plain');
 
-    return;
-  } else if (req.method === 'DELETE' && req.url?.startsWith('/api/users/')) {
-    const userId = req.url.slice(11);
-    const user = users.getUserById(userId);
+        return res.end(`User with id ${userId} does not exist`);
+      }
 
-    if (user === '400') {
-      res.statusCode = 400;
+      users.deleteUser(userId);
+
+      res.statusCode = 204;
       res.setHeader('Content-Type', 'text/plain');
 
-      return res.end('Invalid user id');
-    }
-
-    if (user === '404') {
+      return res.end('Successful removal');
+    } else {
       res.statusCode = 404;
       res.setHeader('Content-Type', 'text/plain');
 
-      return res.end(`User with id ${userId} does not exist`);
+      return res.end('Such url does not exist');
     }
-
-    users.deleteUser(userId);
-
-    res.statusCode = 204;
+  } catch {
+    res.statusCode = 500;
     res.setHeader('Content-Type', 'text/plain');
-
-    return res.end('Successful removal');
-  } else {
-    res.statusCode = 404;
-    res.setHeader('Content-Type', 'text/plain');
-
-    return res.end('Such url does not exist');
+    return res.end('Server error');
   }
 });
 
